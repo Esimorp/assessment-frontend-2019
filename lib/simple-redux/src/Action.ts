1cf44042ae0ac566fc9c4d19f2d325fa8d47fe42
dogsii <dogsii@outlook.com>
2020-09-05 23:23:47 +0800

    pass action test cases

--- lib/simple-redux/src/Action.ts
+++ lib/simple-redux/src/Action.ts
@@ -6,8 +6,20 @@ interface Action {
 type ActionCreator = (payload: unknown) => Action
 
 function createActionCreator(namespace: string, type: string): ActionCreator {
+    if (!namespace)
+        throw Error('namespace must not be null or undefined');
+
+    if (!type)
+        throw Error('type must not be null or undefined');
+
+    if (typeof namespace !== 'string')
+        throw Error('namespace must be string type');
+
+    if (typeof type !== 'string')
+        throw Error('type must be string type');
+
     return (payload: unknown): Action => {
-        return {type: '', payload}
+        return {type: `${namespace}/${type}`, payload}
     }
 }
 
