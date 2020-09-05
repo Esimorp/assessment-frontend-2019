90ba2c18c337f9bb2c8793d279d033c1bf6e00f3
dogsii <dogsii@outlook.com>
2020-09-05 23:58:55 +0800

    pass presenter test cases

--- lib/simple-redux/src/Presenter.ts
+++ lib/simple-redux/src/Presenter.ts
@@ -9,6 +9,35 @@ type Presenter = {
 }
 
 function createPresenter(initState: State = {}, reducers: Record<string, Reducer> = {}, effects: Record<string, Effect> = {}): Presenter {
+    if (!initState)
+        throw Error("initState must not be null or undefined")
+
+    if (typeof initState === 'function')
+        throw Error("initState must not be function")
+
+    if (!reducers)
+        throw Error("reducers must not be null or undefined")
+
+    if (typeof reducers !== 'object')
+        throw Error("reducers must  be object")
+
+    const reducerKeys: string[] = Object.keys(reducers);
+
+    if (reducerKeys.length <= 0)
+        throw Error("reducers must has at least one reducer")
+
+    for (const k of reducerKeys) {
+        if (typeof reducers[k] !== 'function')
+            throw Error("reducers must all be function")
+    }
+
+    const effectKeys: string[] = Object.keys(effects);
+
+    const duplicateKeys = reducerKeys.filter(v => effectKeys.includes(v));
+    if (duplicateKeys.length > 0) {
+        throw Error("there must not be any duplicate keys in reducers and effects");
+    }
+
     return {
         state: initState,
         reducers,
