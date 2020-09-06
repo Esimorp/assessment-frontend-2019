1f961c49f77070501072fdc399e2803520b24e83
dogsii <dogsii@outlook.com>
2020-09-06 23:43:24 +0800

    better Header ui

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,12 @@
-import React    from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react'
+import {Link} from 'react-router-dom'
 
-export function Header () {
-  return (
-    <ul>
-      <li>
-        <Link to="/">
-          Home
-        </Link>
-      </li>
-      <li>
-        <Link to='/create'>Create</Link>
-      </li>
-    </ul>
-  )
+export function Header() {
+    return (
+
+        <ul className="breadcrumb border">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to='/create'>Create</Link></li>
+        </ul>
+    )
 }
