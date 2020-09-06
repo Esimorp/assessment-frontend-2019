3bda0cbce15695819130acea4062d29ed3040f40
dogsii <dogsii@outlook.com>
2020-09-06 20:36:23 +0800

    add Provider to App component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React                              from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import { CreateIncident }                 from './pages/CreateIncident'
-import { Home }                           from './pages/Home'
-import { Header }                         from './components/Header'
+import React from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {CreateIncident} from './pages/CreateIncident'
+import {Home} from './pages/Home'
+import {Header} from './components/Header'
+import {ProviderComponent} from "react-simple-redux";
+import {AppStore} from "./store/AppStore";
 
-function App () {
-  return (
-    <Router>
-      <div className="App">
-        <Header/>
-        <div>
-          <Route exact path="/" component={ Home }/>
-          <Route path="/create" component={ CreateIncident }/>
-        </div>
-      </div>
-    </Router>
-  )
+
+function App() {
+    return (
+        <ProviderComponent store={AppStore}>
+            <Router>
+                <Header/>
+                <Route exact path="/">
+                    {props => <Home {...props} />}
+                </Route>
+                <Route path="/create">
+                    {props => <CreateIncident {...props} />}
+                </Route>
+            </Router>
+        </ProviderComponent>
+    )
 }
 
 export default App
