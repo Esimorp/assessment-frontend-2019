7ac1e9948ae02279cba0f90e4c10f0292fbb38ac
dogsii <dogsii@outlook.com>
2020-09-06 21:19:52 +0800

    Update CreateIncident.js

--- src/pages/CreateIncident.js
+++ src/pages/CreateIncident.js
@@ -1,7 +1,32 @@
 import React from 'react'
+import {connect} from "react-simple-redux";
+import {useForm} from "react-hook-form";
 
-export function CreateIncident () {
-  return (
-    <p>Create Incident form should be here</p>
-  )
+function CreateIncident({createIncident}) {
+    const {register, handleSubmit} = useForm();
+    const onSubmit = data => createIncident(data);
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input name="title" ref={register({required: true})}/>
+            <input name="assignee" ref={register({required: true})}/>
+            <select name="status" ref={register}>
+                <option value="New">New</option>
+                <option value="Assigned">Assigned</option>
+                <option value="In progress">In progress</option>
+                <option value="Pending">Pending</option>
+                <option value="Resolved">Resolved</option>
+                <option value="Closed">Closed</option>
+            </select>
+            <input type="submit"/>
+        </form>
+    );
 }
+
+const CreateIncidentComponent = connect((state) => {
+
+}, (actionWrappers) => {
+    return {createIncident: actionWrappers.Incident.createIncidentAsync}
+})(CreateIncident);
+export {CreateIncidentComponent as CreateIncident}
+
